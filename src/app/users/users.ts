d7e66771789cde9f2b63d6bcff491a8f56438389
Sophie <zsofiliszkai@gmail.com>
2026-04-16 21:24:57 +0200

    users backend hivás done

--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component , inject,  signal } from '@angular/core';
+
+interface User {
+  name: string;
+  id: number;
+}
 
 @Component({
   selector: 'app-users',
@@ -6,4 +12,16 @@ import { Component } from '@angular/core';
   templateUrl: './users.html',
   styleUrl: './users.scss',
 })
-export class Users {}
+export class Users {
+  private http = inject(HttpClient);
+  protected users = signal<User[]>([]);
+
+  ngOnInit(): void {
+    this.http.get<User[]>("/api/users").subscribe(
+      (response) => {
+        this.users.set(response);
+        console.log(response);
+      }
+    );
+  }
+}

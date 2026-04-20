3a07d2c4911a2bf04e0d16347836e0c8371205e5
Sophie <zsofiliszkai@gmail.com>
2026-04-20 12:58:16 +0200

    launch app added

--- src/app/device/device.ts
+++ src/app/device/device.ts
@@ -3,18 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { IMenuItem } from '../types';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-device',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, MatSnackBarModule],
   templateUrl: './device.html',
   styleUrl: './device.scss',
 })
 export class Device implements OnInit {
   private http = inject(HttpClient);
-
   menuItems = signal<IMenuItem[]>([]);
   openFolderId: number | null = null;
+  private snackBar = inject(MatSnackBar);
 
   ngOnInit() {
     this.http.get<IMenuItem[]>('/api/menu').subscribe(items => this.menuItems.set(items));
@@ -22,5 +25,9 @@ export class Device implements OnInit {
 
   toggleFolder(id: number) {
     this.openFolderId = this.openFolderId === id ? null : id;
+  }
+
+  launchApp(name: string) {
+    this.snackBar.open(`${name} elindítva`, 'OK', { duration: 2000 });
   }
 }
